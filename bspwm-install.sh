e596188bab20b5ee314105a2b2a78b6e4e435ff9
Snehit Sah <snehitsah@protonmail.com>
2021-03-07 15:18:03 +0530

    Fix install script

--- bspwm-install.sh
+++ bspwm-install.sh
@@ -4,7 +4,7 @@
 mkdir ~/.local/share/fonts
 cp -R IosevkaTermNerdFontComplete.ttf ~/.local/share/fonts
 cp -R .config/* ~/.config/
-chmod -R +x ~/.config/bspwm/scripts
+chmod -R +x ~/.config/bspwm
 chmod -R +x ~/.config/polybar/scripts
 sudo pacman -Syu --needed --noconfirm - < packages-repository.txt
 dbus-launch dconf load / < xed.dconf
