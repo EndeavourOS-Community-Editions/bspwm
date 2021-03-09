7fd97346cf0ef9fdf2c8a9b7bdd1b54256db0962
OdiousImp2604 <74424076+OdiousImp2604@users.noreply.github.com>
2021-03-09 16:34:20 +0000

    Added Keyhint.sh

---
+++ .config/polybar/scripts/keyhint.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+yad --title="EndeavourOS bspwm-wm keybindings:" --no-buttons --geometry=400x345-15-400 --list --column=key: --column=description: --column=command: "ESC" "close this app" "" "=" "modkey" "(set mod Mod4)" "+enter" "open a terminal" "" "+w" "open Browser" "" "+n" "open Filebrowser" "" "+d" "app menu" "(rofi)" "+Shift+q" "close focused app" "(kill)" "Print-key" "screenshot" "(scrot)" "+Shift+e" "logout menu" "(rofi)" "+F1" "open keybinding helper" "full list" "+Alt+r" "reload bspwm" "bpsc restart" "+ESC" "reload sxhkd" "pkill -USR1 -x sxhkd"
