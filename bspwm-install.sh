17d87c1f52b3c840c8a5f7f0e46c14608cbff691
Snehit Sah <48585950+snehitsah@users.noreply.github.com>
2021-03-27 19:24:07 +0530

    Update font cache on manual install

--- bspwm-install.sh
+++ bspwm-install.sh
@@ -7,4 +7,5 @@ cp -R .config/* ~/.config/
 chmod -R +x ~/.config/bspwm
 chmod -R +x ~/.config/polybar/scripts
 sudo pacman -Syu --needed --noconfirm - < packages-repository.txt
+sudo fc-cache -f -v
 dbus-launch dconf load / < xed.dconf
