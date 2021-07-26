58a4845ca96c929b9fb578d99cb6b79b5f93207d
Snehit Sah <snehitsah@protonmail.com>
2021-07-26 18:32:17 +0530

    Fix checkupdates script
    Now shows 0 for no updates
    
    Fixes #8

--- .config/polybar/scripts/checkupdates.sh
+++ .config/polybar/scripts/checkupdates.sh
@@ -2,8 +2,13 @@
 
 updates=$(checkupdates)
 
-printf '%s\n' "$updates" > /tmp/bspwm-eos-polybar-updates
-
-count=$(wc -l "/tmp/bspwm-eos-polybar-updates" | awk '{print $1}')
+if [ -z "$updates" ]
+then
+  count=0
+else
+  printf '%s\n' "$updates" > /tmp/bspwm-eos-polybar-updates
+  count=$(wc -l "/tmp/bspwm-eos-polybar-updates" | awk '{print $1}')
+fi
 
 echo "%{T1}%{T-} %{T2}$count%{T-}"
+
