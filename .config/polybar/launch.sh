07c6be68603b7c2ff02d4f2296d36d9b6dc5f2ff
Snehit Sah <snehitsah@protonmail.com>
2022-07-18 10:33:57 +0530

    Replace tilde with HOME
    Signed-off-by: Snehit Sah <snehitsah@protonmail.com>

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,4 +7,4 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar
-polybar top -c ~/.config/polybar/config.ini &
+polybar top -c $HOME/.config/polybar/config.ini &
--- bspwm-install.sh
+++ bspwm-install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Easy setup for testing
-mkdir ~/.local/share/fonts
-cp -R IosevkaTermNerdFontComplete.ttf ~/.local/share/fonts
-cp -R .config/* ~/.config/
-chmod -R +x ~/.config/bspwm
-chmod -R +x ~/.config/polybar/scripts
+mkdir $HOME/.local/share/fonts
+cp -R IosevkaTermNerdFontComplete.ttf $HOME/.local/share/fonts
+cp -R .config/* $HOME/.config/
+chmod -R +x $HOME/.config/bspwm
+chmod -R +x $HOME/.config/polybar/scripts
 sudo pacman -Syu --needed --noconfirm - < packages-repository.txt
 sudo fc-cache -f -v
 dbus-launch dconf load / < xed.dconf
